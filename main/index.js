d5dc56807a69933ed45f00b45a807f4ae736ac79
Alnatra23 <naufaljaya171@gmail.com>
2023-01-24 11:09:48 +0700

    LearnNode

---
+++ main/index.js
@@ -0,0 +1,7 @@
+const express = require("express/lib/config");
+const nodemon = require("nodemon/lib/config");
+const bodyParser = require("body-parser/lib/config");
+const cors = require("cors/lib/config");
+const app = express();
+
+required;
